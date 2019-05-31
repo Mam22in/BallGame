0346bb99f4d495b4db06721b892af4b38773105f
Mam22in <mam22in@gmail.com>
2019-06-01 00:24:38 +0300

    Add files via upload

---
+++ Assets/Scripts/BallControler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    Rigidbody rb; 
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = " ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(x,0,z);
+        rb.AddForce(velocity * speed);
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 13)
+        {
+            winText.text = "You Won!";
+        }
+    }
+}
---
+++ Assets/Scripts/CameraControler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 offset;
+    public GameObject ball;
+
+    void Start()
+    {
+        offset = transform.position- ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + offset; 
+    }
+}
